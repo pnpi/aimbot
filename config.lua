9ccdc0c51ec288a69108dc55e5c7b993704406c1
pnpi <fargosog@gmail.com>
2024-07-11 21:01:08 -0400

    Set up global environment for easy configuration.

---
+++ config.lua
@@ -0,0 +1,17 @@
+getgenv().Config = {
+    Aim = {
+        Key = Enum.KeyCode.C, 
+        TweenTime = 0.135,
+        EasingStyle = Enum.EasingStyle.Sine,
+        EasingDirection = Enum.EasingDirection.InOut
+    },
+
+    FOV = {
+        Color = Color3.fromRGB(0, 255, 255),
+        Filled = false,
+        Radius = 150,
+        Visible = true,
+        Thickness = 1,
+        Transparency = 1 
+    }
+}
