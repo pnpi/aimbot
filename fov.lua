50619208edfde43a90abfe1d562e3ff1fd432d56
pnpi <fargosog@gmail.com>
2024-07-11 21:23:51 -0400

    set up for render on every frame.

--- fov.lua
+++ fov.lua
@@ -12,3 +12,58 @@ if Debug then
   print(FOV.Thickness)
   print(FOV.Transparency)
 end
+
+local FOV_Class = {}
+
+FOV_Class.Circle = Drawing.new("Circle")
+
+function FOV_Class.SetColor()
+    if FOV_Class.Circle then
+        FOV_Class.Circle.Color = FOV.Color
+    end
+end
+
+function FOV_Class.SetFilled()
+    if FOV_Class.Circle then 
+        FOV_Class.Circle.Filled = FOV.Filled
+    end
+end
+
+function FOV_Class.SetRadius()
+    if FOV_Class.Circle then 
+        FOV_Class.Circle.Radius = FOV.Radius
+    end
+end
+
+function FOV_Class.SetVisible()
+    if FOV_Class.Circle then 
+        FOV_Class.Circle.Visible = FOV.Visible
+    end
+end
+
+function FOV_Class.SetThickness()
+    if FOV_Class.Circle then 
+        FOV_Class.Circle.Thickness = FOV.Thickness
+    end 
+end
+
+function FOV_Class.SetTransparency()
+    if FOV_Class.Circle then 
+        FOV_Class.Circle.Transparency = FOV.Transparency
+    end
+end
+
+function FOV_Class.onRenderStepped()
+    if FOV_Class.Circle then 
+        FOV_Class.SetColor()
+        FOV_Class.SetFilled()
+        FOV_Class.SetRadius()
+        FOV_Class.SetVisible()
+        FOV_Class.SetThickness()
+        FOV_Class.SetTransparency()
+    else
+      if Debug then 
+          print("Circle is nil, unable to set parameters.")
+      end
+    end
+end
