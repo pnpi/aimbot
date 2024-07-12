293205d66e40ab9c05f7abc70c358d5ba4050781
pnpi <fargosog@gmail.com>
2024-07-11 21:10:32 -0400

    Store config property into a variable.

---
+++ fov.lua
@@ -0,0 +1,13 @@
+local Get = loadstring(game:HttpGet("https://raw.githubusercontent.com/pnpi/aimbot/main/config.lua"))()
+local Config = getgenv().Config 
+
+local Debug = true
+
+if Debug then 
+  print(FOV.Color)
+  print(FOV.Filled)
+  print(FOV.Radius)
+  print(FOV.Visible)
+  print(FOV.Thickness)
+  print(FOV.Transparency)
+end
